eef6c44f096c916966057f9cb9e5802dc504c497
KTangg <k.tangtangtang@gmail.com>
2026-01-30 21:41:20 +0700

    add type PATH

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::os::unix::fs::PermissionsExt;
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
@@ -7,6 +8,9 @@ fn main() {
     loop {
          print!("$ ");
          io::stdout().flush().unwrap();
+
+        let path_env = std::env::var("PATH").unwrap_or_default();
+        let path: Vec<&str> = path_env.split(':').collect();
 
         let mut command = String::new();
         io::stdin().read_line(&mut command).unwrap();
@@ -18,15 +22,28 @@ fn main() {
                 println!("{}", parts.next().unwrap_or(""));
             }
             Some("type") => {
-                if let Some(arg) = parts.next() {
-                    if ["exit", "echo", "type"].contains(&arg) {
-                        println!("{} is a shell builtin", arg);
-                    } else {
-                        println!("{}: not found", arg);
-                    }
-                }
+                builtin_type(parts.next().unwrap_or(""), &path);
             }
             _ => println!("{}: command not found", command.trim())
         }
+    }
+}
+
+fn builtin_type(cmd: &str, path: &Vec<&str>) {
+    if ["exit", "echo", "type"].contains(&cmd) {
+        println!("{} is a shell builtin", cmd);
+    } else {
+        for dir in path {
+            let full_path = format!("{}/{}", dir, cmd);
+            // Check if the file
+            if std::path::Path::new(&full_path).exists() && std::path::Path::new(&full_path).is_file() {
+                let metadata = std::fs::metadata(&full_path).unwrap();
+                if metadata.permissions().mode() & 0o111 != 0 {
+                    println!("{} is {}", cmd, full_path);
+                    return;
+                }
+            }
+        }
+        println!("{}: not found", cmd);
     }
 }
